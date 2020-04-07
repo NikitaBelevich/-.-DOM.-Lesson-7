284f9eeaf8981e236f3550401cf2a692d13f5d76
Nikita Belevich <belevish_1998@mail.ru>
2020-04-07 23:25:06 +0300

    Сделана 1 задача

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@
 
 
 // Task 1. Очистите элемент. Создайте функцию clear(elem), которая удаляет всё содержимое из elem.
+let olElem = document.querySelector('.task1').lastElementChild;
+let but1 = document.querySelector('.task1 button');
+but1.addEventListener('click', () => {
+    clear(olElem);
+});
+
+function clear(elem) {
+    elem.firstElementChild.remove();
+    elem.lastElementChild.remove();
+}
 
 
 
