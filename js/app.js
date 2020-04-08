2bd8a921d53b6bc60f7f70033d7bc09232119102
Nikita Belevich <belevish_1998@mail.ru>
2020-04-08 09:53:51 +0300

    Сделана 2 задача
    Скрипт задачи динамически генерирует списки до того как юзер вводит данные

--- js/app.js
+++ js/app.js
@@ -14,10 +14,43 @@ function clear(elem) {
 }
 
 
+/* 
+Task 2. Создайте список 
+Напишите интерфейс для создания списка. 
 
+Для каждого пункта:
+Запрашивайте содержимое пункта у пользователя с помощью prompt.
+Создавайте элемент li и добавляйте его к ul.
+Процесс прерывается, когда пользователь нажимает Esc или вводит пустую строку
+Все элементы должны создаваться динамически.
 
+Если пользователь вводит HTML-теги -– пусть в списке они показываются как обычный текст.
+*/
+let divTask2 = document.querySelector('.task2');
+const but2 = document.querySelector('.task2 button');
+but2.addEventListener('click', () => {
+    generateList(divTask2);
+});
 
+function generateList(targetElement) {
+    const title = document.createElement('h3');
+    title.textContent = 'Create List';
+    targetElement.append(title);
 
+    const list = document.createElement('ul');
+    targetElement.append(list);
+    while (true) {
+        let textLi = prompt('Введите текст для пункта списка', '');
+        if (!textLi) {
+            if (list.children.length == 0) list.remove();
+            throw new Error('Отмена генерации списка');
+        }
+
+        const li = document.createElement('li');
+        li.textContent = textLi;
+        list.append(li);
+    }
+}
 
 
 
